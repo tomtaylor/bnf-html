b24afa595f9ced7ca23d87e5cd4ddd5340951d40
Tom Taylor <tom@tomtaylor.co.uk>
2012-10-08 19:02:33 +0100

    Updated fetch script to wait 10 seconds between requests.

--- fetch.sh
+++ fetch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-wget --load-cookies cookies.txt --mirror --no-parent --convert-links --page-requisites http://www.medicinescomplete.com/mc/bnf/current/
+wget --load-cookies cookies.txt --mirror --no-parent --convert-links --wait=10 --page-requisites http://www.medicinescomplete.com/mc/bnf/current/
